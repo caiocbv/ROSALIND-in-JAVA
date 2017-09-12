c45fc4efc363cc8d972f1c55de9b11291ad08957
Caio <caiocbv@gmail.com>
2017-09-12 11:18:27 -0300

    all

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.util.Set;
  * Rosalind is a platform for learning bioinformatics and programming through problem solving. Find More at:
 	http://rosalind.info
 
-	This project has anwsers from this problems:
+	This project has anwsers from these problems:
 
 	1) http://rosalind.info/problems/dna/
 	2) http://rosalind.info/problems/rna/
