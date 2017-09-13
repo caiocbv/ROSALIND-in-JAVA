d512b145b08d123909068a45c8a21ff1fd186d38
Caio <caiocbv@gmail.com>
2017-09-13 09:48:59 -0300

    http://rosalind.info/problems/ksim/

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
 		System.out.println("5) http://rosalind.info/problems/prot/");
 		System.out.println("6) http://rosalind.info/problems/cons/");
 		System.out.println("7) http://rosalind.info/problems/orf/");
-		System.out.println("8) ");
+		System.out.println("8) http://rosalind.info/problems/ksim/");
 		
 		Scanner s = new Scanner(System.in);
 		int x = s.nextInt();
@@ -67,9 +67,107 @@ public class Main {
 				break;
 
 			case 8:
+				Main.ksim();
 				break;
 		}
 		
+	}
+
+	private static void ksim() {
+		try {
+			Scanner sc = new Scanner(new File("entrada8.txt"));
+			String s = sc.nextLine();
+			int k = Integer.parseInt(s);
+			String motif = sc.nextLine();
+			s = sc.nextLine();
+			sc.close();
+			
+			PrintWriter saida = new PrintWriter(new File("saida8.txt"));
+			
+			for(int i = 0; i < s.length();i+=3){
+				String temp = "";
+				for(int j = i; j < s.length();j++){
+					temp = temp + s.charAt(j);
+					int d = Main.EditDistance(motif, temp);
+					//System.out.println("A Distancia é Tal: " + d + " Na posicao: i = " + (i+1) + " j = "+ (j+1));
+					if(d <= k){
+						saida.println((i+1) + " "+  (j+1));
+					}
+				}
+				
+			}
+			
+			saida.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static int EditDistance(String s1, String s2) {
+		if (s1 == null) {
+            throw new NullPointerException("s1 must not be null");
+        }
+
+        if (s2 == null) {
+            throw new NullPointerException("s2 must not be null");
+        }
+
+        if (s1.equals(s2)) {
+            return 0;
+        }
+
+        if (s1.length() == 0) {
+            return s2.length();
+        }
+
+        if (s2.length() == 0) {
+            return s1.length();
+        }
+
+        // create two work vectors of integer distances
+        int[] v0 = new int[s2.length() + 1];
+        int[] v1 = new int[s2.length() + 1];
+        int[] vtemp;
+
+        // initialize v0 (the previous row of distances)
+        // this row is A[0][i]: edit distance for an empty s
+        // the distance is just the number of characters to delete from t
+        for (int i = 0; i < v0.length; i++) {
+            v0[i] = i;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            // calculate v1 (current row distances) from the previous row v0
+            // first element of v1 is A[i+1][0]
+            //   edit distance is delete (i+1) chars from s to match empty t
+            v1[0] = i + 1;
+
+            // use formula to fill in the rest of the row
+            for (int j = 0; j < s2.length(); j++) {
+                int cost = 1;
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    cost = 0;
+                }
+                v1[j + 1] = Math.min(
+                        v1[j] + 1,              // Cost of insertion
+                        Math.min(
+                                v0[j + 1] + 1,  // Cost of remove
+                                v0[j] + cost)); // Cost of substitution
+            }
+
+            // copy v1 (current row) to v0 (previous row) for next iteration
+            //System.arraycopy(v1, 0, v0, 0, v0.length);
+
+            // Flip references to current and previous row
+            vtemp = v0;
+            v0 = v1;
+            v1 = vtemp;
+
+        }
+
+        return v0[s2.length()];
 	}
 
 	private static void orf() {
